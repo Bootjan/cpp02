a8d9daadc8666bc19c8417cb18cd8b5ba8e94ace
Bote Schaafsma <bootjan@Botes-MacBook-Pro.local>
2024-01-11 13:11:08 +0100

    update

--- ex01/Fixed.cpp
+++ ex01/Fixed.cpp
@@ -6,22 +6,54 @@
 /*   By: bootjan <bootjan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 14:04:52 by bschaafs          #+#    #+#             */
-/*   Updated: 2024/01/10 22:20:08 by bootjan          ###   ########.fr       */
+/*   Updated: 2024/01/11 11:46:53 by bootjan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 
+const static float	dividers[] = { 0.5f, 0.25f, 0.125f, 0.0625f, \
+0.03125f, 0.0156125f, 0.007806125f, 0.0039030625f};
+
 static void	print_message(const std::string msg)
 {
-	if (OUTPUT)
-		std::cout << msg << std::endl;
+	std::cout << msg << std::endl;
 }
 
 Fixed::Fixed(void)
 {
 	print_message("Default constructor called");
 	this->_value = 0;
+}
+
+Fixed::Fixed(const int Int)
+{
+	print_message("Int constructor called");
+	this->_value = Int << BIT_OFFSET;
+}
+
+Fixed::Fixed(const float Float)
+{
+	print_message("Float constructor called");
+	int		roundedInt = (int)floor(Float);
+	float	decimals = Float - roundedInt;
+
+	this->_value = roundedInt << BIT_OFFSET;
+	for (int i = 1; i <= BIT_OFFSET; i++)
+	{
+		std::cout << "decimals: " << decimals << std::flush;
+		std::cout << "	conditional: " << 0.5f / i << std::endl;
+		if (decimals >= dividers[i - 1])
+		{
+			this->_value |= (1 << (BIT_OFFSET - i));
+			decimals -= dividers[i - 1];
+		}
+	}
+}
+
+Fixed::~Fixed()
+{
+	print_message("Destructor called");
 }
 
 Fixed&	Fixed::operator=(const Fixed& original)
@@ -31,9 +63,10 @@ Fixed&	Fixed::operator=(const Fixed& original)
 	return (*this);
 }
 
-Fixed::~Fixed()
+std::ostream&	operator<<(std::ostream& o, const Fixed& fixed)
 {
-	print_message("Destructor called");
+	o << fixed.toFloat();
+	return (o);
 }
 
 Fixed::Fixed(const Fixed &fixed)
@@ -51,4 +84,22 @@ int	Fixed::getRawBits(void) const
 void	Fixed::setRawBits(const int raw)
 {
 	this->_value = raw;
+}
+
+float	Fixed::toFloat(void) const
+{
+	int		Int = this->_value >> BIT_OFFSET;
+	float	decimals = 0;
+
+	for (int i = 1; i <= BIT_OFFSET; i++)
+	{
+		if (this->_value & (1 << (BIT_OFFSET - i)))
+			decimals += dividers[i - 1];
+	}
+	return ((float)Int + decimals);
+}
+
+int		Fixed::toInt(void) const
+{
+	return (this->_value >> BIT_OFFSET);
 }
--- ex01/Fixed.hpp
+++ ex01/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: bootjan <bootjan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 14:05:04 by bschaafs          #+#    #+#             */
-/*   Updated: 2024/01/10 22:22:20 by bootjan          ###   ########.fr       */
+/*   Updated: 2024/01/11 11:46:10 by bootjan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,30 @@
 
 # include <iostream>
 
+# ifndef OUTPUT
+#  define OUTPUT 1
+# endif
+
+# define BIT_OFFSET 8
+
 class Fixed
 {
 	private:
 		int					_value;
-		static const int	_nBits = BIT_OFFSET;
+		static const int	_nBits = 8;
 	public:
 		Fixed(void);
+		Fixed(const int Int);
+		Fixed(const float Float);
 		Fixed(const Fixed &fixed);
-		Fixed& operator=(const Fixed& original);
+		Fixed& 			operator=(const Fixed& original);
 		~Fixed();
 		int		getRawBits(void) const;
 		void	setRawBits(const int raw);
+		float	toFloat(void) const;
+		int		toInt(void) const;
 };
+
+std::ostream&	operator<<(std::ostream& o, Fixed const& fixed);
 
 #endif
